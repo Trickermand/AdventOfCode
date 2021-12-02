a21c48120c950d0819223811f6a0a0d367c7d0c4
Patrick (Splatt) Hansen <ph@siteshop.dk>
2021-12-02 16:10:23 +0100

    Year2020 - Implemented solution for Day1 Part1 and Part2

--- Year2020/Day1.cs
+++ Year2020/Day1.cs
@@ -11,20 +11,41 @@ namespace Year2020
 
         public static void Part1()
         {
-            List<string> inputs = IO.ReadInput(Day);
+            List<int> inputs = IO.ConvertStringListToIntList(IO.ReadInput(Day));
 
+            int result = 0;
 
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                for (int j = i + 1; j < inputs.Count; j++)
+                {
+                    if (inputs[i] + inputs[j] == 2020)
+                        result = inputs[i] * inputs[j];
+                }
+            }
 
-            Console.WriteLine($"{Day} {MethodBase.GetCurrentMethod().Name} answer: ");
+            Console.WriteLine($"{Day} {MethodBase.GetCurrentMethod().Name} answer: {result}");
         }
 
         public static void Part2()
         {
-            List<string> inputs = IO.ReadInput(MethodBase.GetCurrentMethod().DeclaringType.Name);
+            List<int> inputs = IO.ConvertStringListToIntList(IO.ReadInput(Day));
 
+            int result = 0;
 
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                for (int j = i + 1; j < inputs.Count; j++)
+                {
+                    for (int k = j + 1; k < inputs.Count; k++)
+                    {
+                        if (inputs[i] + inputs[j] + inputs[k] == 2020)
+                            result = inputs[i] * inputs[j] * inputs[k];
+                    }
+                }
+            }
 
-            Console.WriteLine($"{Day} {MethodBase.GetCurrentMethod().Name} answer: ");
+            Console.WriteLine($"{Day} {MethodBase.GetCurrentMethod().Name} answer: {result}");
         }
     }
 }
