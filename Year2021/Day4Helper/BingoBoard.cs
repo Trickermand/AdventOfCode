d2062202f7999f93fd79d8d50aa1efddcf3aa206
Trickermand <Patrickhansen2500@hotmail.com>
2021-12-04 11:46:55 +0100

    Year2021 -Day4 Implemented BingoBoard and BingoCell

---
+++ Year2021/Day4Helper/BingoBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Year2021.Day4Helper
+{
+    public class BingoBoard
+    {
+        public BingoCell[][] Board { get; set; }
+        public BingoBoard(List<string> inputBoard)
+        {
+            BingoCell[][] tmp = new BingoCell[5][];
+
+            for (int i = 0; i < inputBoard.Count; i++)
+            {
+                List<string> inputLine = inputBoard[i].Split(' ').Where(x => x != " ").ToList();
+                tmp[i] = new BingoCell[5];
+
+                for (int j = 0; j < inputLine.Count; j++)
+                {
+                    int num = int.Parse(inputLine[i]);
+                    tmp[i][j] = new BingoCell(num);
+                }
+            }
+
+            Board = tmp;
+        }
+
+        public void SetCell()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Year2021/Day4Helper/BingoCell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Year2021.Day4Helper
+{
+    public class BingoCell
+    {
+        public int Number { get; set; }
+        public bool IsHit { get; set; }
+
+        public BingoCell(int num)
+        {
+            Number = num;
+            IsHit = false;
+        }
+
+    }
+}
