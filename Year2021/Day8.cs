8c2e6d31c1240152488d60d26953674e4e4f09de
Patrick (Splatt) Hansen <ph@siteshop.dk>
2021-12-08 14:36:27 +0100

    Year2021 - Optimized Day8 Part1 and added helper class for Part2

--- Year2021/Day8.cs
+++ Year2021/Day8.cs
@@ -21,24 +21,17 @@ namespace Year2021
             List<string> inputs = IO.ReadInputAsLines(Day);
             int result = 0;
             
-
             foreach (var line in inputs)
             {
                 List<string> segments = line.Split("|")[1].Split(" ").Where(x => x != "").ToList();
-                int count1 = 0, count4 = 0, count7 = 0, count8 = 0;
                 for (int i = 0; i < segments.Count; i++)
                 {
-                    if (segments[i].Length == 2)
-                        count1++;
-                    if (segments[i].Length == 4)
-                        count4++;
-                    if (segments[i].Length == 3)
-                        count7++;
-                    if (segments[i].Length == 7)
-                        count8++;
+                    if (segments[i].Length == 2
+                        || segments[i].Length == 3
+                        || segments[i].Length == 4
+                        || segments[i].Length == 7)
+                        result++;
                 }
-
-                result += count1 + count4 + count7 + count8;
             }
 
             Console.WriteLine($"{Day} {MethodBase.GetCurrentMethod().Name} answer: {result}");
---
+++ Year2021/Day8Helper/SegmentDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Year2021.Day8Helper
+{
+    class SegmentDisplay
+    {
+        public char Top = ' ';
+        public char TopLeft = ' ';
+        public char TopRight = ' ';
+        public char Middle = ' ';
+        public char BottomLeft = ' ';
+        public char BottomRight = ' ';
+        public char Bottom = ' ';
+
+
+    }
+}
