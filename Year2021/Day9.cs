ea51669871166524852bd1dfdc3ed0e0c678db73
Patrick (Splatt) Hansen <ph@siteshop.dk>
2022-03-02 15:46:02 +0100

    Year2021 - Implemented day 9 part 1

--- Year2021/Day9.cs
+++ Year2021/Day9.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Utilities;
+using Year2021.Day9Helper;
 
 namespace Year2021
 {
@@ -21,12 +22,15 @@ namespace Year2021
             List<string> inputs = IO.ReadInputAsLines(Day);
             int result = 0;
 
-            for (int i = 0; i < inputs.Count; i++)
+            HeightMap map = new HeightMap(inputs);
+            for (int i = 0; i < map.Area.Length; i++)
             {
-                for (int j = 0; j < inputs[i].Length; j++)
+                for (int j = 0; j < map.Area[i].Length; j++)
                 {
-                    if (IsLowestAdjacent(inputs, i, j))
-                        result += int.Parse(inputs[i][j].ToString()) + 1;
+                    if (map.IsLowestPoint(i, j))
+                    {
+                        result += int.Parse(map.Area[i][j].ToString()) + 1;
+                    }
                 }
             }
 
@@ -43,23 +47,5 @@ namespace Year2021
             Console.WriteLine($"{Day} {MethodBase.GetCurrentMethod().Name} answer: {result}");
         }
 
-        private static bool IsLowestAdjacent(List<string> input, int rowIndex, int columnIndex)
-        {
-            if (columnIndex > 0 && int.Parse(input[rowIndex][columnIndex].ToString()) > int.Parse(input[rowIndex][columnIndex - 1].ToString()))
-                return false;
-            
-            if (columnIndex < input[rowIndex].Length - 1 && int.Parse(input[rowIndex][columnIndex].ToString()) > int.Parse(input[rowIndex][columnIndex + 1].ToString()))
-                return false;
-
-            if (rowIndex > 0 && int.Parse(input[rowIndex][columnIndex].ToString()) > int.Parse(input[rowIndex - 1][columnIndex].ToString()))
-                return false;
-
-            if (rowIndex < input.Count - 1 && int.Parse(input[rowIndex][columnIndex].ToString()) > int.Parse(input[rowIndex + 1][columnIndex].ToString()))
-                return false;
-
-
-
-            return true;
-        }
     }
 }
---
+++ Year2021/Day9Helper/HeightMap.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Year2021.Day9Helper
+{
+    class HeightMap
+    {
+        public string[] Area { get; set; }
+
+        public HeightMap(List<string> input)
+        {
+            Area = new string[input.Count];
+            for (int i = 0; i < input.Count; i++)
+            {
+                Area[i] = input[i];
+            }
+        }
+
+        public bool IsLowestPoint(int i, int j)
+        {
+            return IsLowerThanAbove(i, j) && IsLowerThanBelow(i, j) && IsLowerThanLeft(i, j) && IsLowerThanRight(i, j);
+        }
+
+        public bool IsLowerThanAbove(int i, int j)
+        {
+            if (i < 1) return true;
+            if (Area[i][j] < Area[i - 1][j]) return true;
+            return false;
+        }
+
+        public bool IsLowerThanBelow(int i, int j)
+        {
+            if (i + 1 >= Area.Length) return true;
+            if (Area[i][j] < Area[i + 1][j]) return true;
+            return false;
+        }
+
+        public bool IsLowerThanLeft(int i, int j)
+        {
+            if (j < 1) return true;
+            if (Area[i][j] < Area[i][j - 1]) return true;
+            return false;
+        }
+
+        public bool IsLowerThanRight(int i, int j)
+        {
+            if (j + 1 >= Area[i].Length) return true;
+            if (Area[i][j] < Area[i][j + 1]) return true;
+            return false;
+        }
+
+    }
+}
