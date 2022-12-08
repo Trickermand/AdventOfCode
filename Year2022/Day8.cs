2ba1fdbdb208e3f6cee4ae66d0bf3e7a207b2140
Patrick (Splatt) Hansen <ph@siteshop.dk>
2022-12-08 10:05:38 +0100

    Year 2022 - Created setup for Day 8

---
+++ Year2022/Day8.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using Utilities;
+
+namespace Year2022
+{
+    public static class Day8
+    {
+        private static readonly string Day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void All()
+        {
+            Part1();
+            Part2();
+        }
+
+        public static void Part1()
+        {
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+
+
+            Console.WriteLine($"{Day} {MethodName} answer: {result}");
+        }
+
+        public static void Part2()
+        {
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+
+
+            Console.WriteLine($"{Day} {MethodName} answer: {result}");
+        }
+    }
+}
