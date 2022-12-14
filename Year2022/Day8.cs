2d8dc017925f1ffe13063da816a703f02e707af2
Patrick (Splatt) Hansen <ph@siteshop.dk>
2022-12-14 12:58:50 +0100

    Year 2022 - Implemented Day 8 part 2

--- Year2022/Day8.cs
+++ Year2022/Day8.cs
@@ -38,7 +38,6 @@ namespace Year2022
                 for (int j = 0; j < forest[i].Count; j++)
                 {
                     int tree = forest[i][j];
-                    bool pass = false;
                     bool visible = true;
                     if (i == 0 || j == 0 || i == forest.Count - 1 || j == forest[i].Count - 1)
                     {
@@ -109,7 +108,66 @@ namespace Year2022
             List<string> inputs = IO.ReadInputAsLines(Day);
             int result = 0;
 
+            List<List<int>> forest = new();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                List<int> line = new();
+                for (int j = 0; j < inputs[i].Length; j++)
+                {
+                    line.Add(int.Parse(inputs[i][j].ToString()));
+                }
+                forest.Add(line);
+            }
 
+            int bestScore = 0;
+
+            for (int i = 0; i < forest.Count; i++)
+            {
+                for (int j = 0; j < forest[i].Count; j++)
+                {
+                    int treeCountN = 0;
+                    int treeCountS = 0;
+                    int treeCountE = 0;
+                    int treeCountW = 0;
+                    int tree = forest[i][j];
+                    if (i == 0 || j == 0 || i == forest.Count - 1 || j == forest[i].Count - 1)
+                        continue;
+
+                    for (int k = i - 1; k >= 0; k--)
+                    {
+                        treeCountN++;
+                        if (tree <= forest[k][j])
+                            break;
+                    }
+
+                    for (int k = i + 1; k < forest.Count; k++)
+                    {
+                        treeCountS++;
+                        if (tree <= forest[k][j])
+                            break;
+                    }
+
+                    for (int k = j + 1; k < forest[i].Count; k++)
+                    {
+                        treeCountE++;
+                        if (tree <= forest[i][k])
+                            break;
+                    }
+
+                    for (int k = j - 1; k >= 0; k--)
+                    {
+                        treeCountW++;
+                        if (tree <= forest[i][k])
+                            break;
+                    }
+
+                    int score = treeCountN * treeCountS * treeCountE * treeCountW;
+                    if (bestScore < score)
+                        bestScore = score;
+                }
+            }
+
+            result = bestScore;
 
             Console.WriteLine($"{Day} {MethodName} answer: {result}");
         }
