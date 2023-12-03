3a4ea253b67cb5ad935884d04c084f557f248565
patrick hansen <patrickhansen2500@hotmail.com>
2023-12-03 23:03:17 +0100

    Year 2023 - Setup for Day3 Part1

---
+++ Year2023/Day3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using Utilities;
+using System.Diagnostics;
+
+namespace Year2023
+{
+    public static class Day3
+    {
+        private static readonly string Day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void All()
+        {
+            Part1();
+            Part2();
+        }
+
+        public static void Part1()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> lines = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+
+                }
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Day} {MethodName} answer: {result} - in {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        public static void Part2()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Day} {MethodName} answer: {result} - in {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
