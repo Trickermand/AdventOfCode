7d979a584a889bd699a86548cc8a7e981a394a80
patrick hansen <patrickhansen2500@hotmail.com>
2024-12-01 19:50:11 +0100

    Year 2024  - Implemented Day 1

---
+++ Year2024/Day1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using Utilities;
+using System.Diagnostics;
+
+namespace Year2024
+{
+    public static class Day1
+    {
+        private static readonly string Day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void All()
+        {
+            Part1();
+            Part2();
+        }
+
+        public static void Part1()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+            List<int> firsts = new();
+            List<int> seconds = new();
+
+            foreach (var line  in inputs)
+            {
+                var segments = line.Split("   ");
+                firsts.Add(int.Parse(segments[0]));
+                seconds.Add(int.Parse(segments[1]));
+            }
+
+            firsts = firsts.Order().ToList();
+            seconds = seconds.Order().ToList();
+
+            for (int i = 0; i < firsts.Count; i++)
+            {
+                result += Math.Abs(firsts[i] - seconds[i]);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Day} {MethodName} answer: {result} - in {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        public static void Part2()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+            List<int> firsts = new();
+            List<int> seconds = new();
+
+            foreach (var line in inputs)
+            {
+                var segments = line.Split("   ");
+                firsts.Add(int.Parse(segments[0]));
+                seconds.Add(int.Parse(segments[1]));
+            }
+
+            for (int i = 0;i < firsts.Count;i++)
+            {
+                int counts = 0;
+                for (int j = 0; j < seconds.Count; j++)
+                {
+                    if (firsts[i] == seconds[j])
+                        counts++;
+                }
+                result += counts * firsts[i];
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Day} {MethodName} answer: {result} - in {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ Year2024/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Year2024
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("===== Running Advent of Code 2024 =====");
+
+            RunAll();
+
+            Console.WriteLine("Complete ...");
+        }
+
+        static void RunAll()
+        {
+            string namespaceName = typeof(Program).Namespace;
+            var classes = System.Reflection.Assembly.GetExecutingAssembly().GetTypes().Where(x => x.Namespace == namespaceName && Regex.IsMatch(x.Name, @"Day(\d{1}|\d{2})"));
+
+            foreach (var day in classes)
+            {
+                var parts = day.GetMethods().Where(x => Regex.IsMatch(x.Name, @"Part(\d{1})"));
+                foreach (var part in parts)
+                {
+                    part.Invoke(null, null);
+                }
+            }
+        }
+    }
+}
