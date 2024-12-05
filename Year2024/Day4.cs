9d7d97f294c85e81dd75e7b3c0a131dc16b30f65
Patrick (Splatt) Hansen <ph@siteshop.dk>
2024-12-05 15:50:35 +0100

    Year 2024 - Day4 Part1 implemented

---
+++ Year2024/Day4.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using Utilities;
+using System.Diagnostics;
+using System.Text;
+
+namespace Year2024
+{
+    public static class Day4
+    {
+        private static readonly string Day = MethodBase.GetCurrentMethod().DeclaringType.Name;
+
+        public static void All()
+        {
+            Part1();
+            Part2();
+        }
+
+        public static void Part1()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                for (int j = 0; j < inputs[i].Length; j++)
+                {
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i][j + 1] == 'M' && inputs[i][j + 2] == 'A' && inputs[i][j + 3] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i + 1][j + 1] == 'M' && inputs[i + 2][j + 2] == 'A' && inputs[i + 3][j + 3] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i + 1][j] == 'M' && inputs[i + 2][j] == 'A' && inputs[i + 3][j] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i + 1][j - 1] == 'M' && inputs[i + 2][j - 2] == 'A' && inputs[i + 3][j - 3] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i][j - 1] == 'M' && inputs[i][j - 2] == 'A' && inputs[i][j - 3] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i - 1][j - 1] == 'M' && inputs[i - 2][j - 2] == 'A' && inputs[i - 3][j - 3] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i - 1][j] == 'M' && inputs[i - 2][j] == 'A' && inputs[i - 3][j] == 'S') ? 1 : 0;
+                    result += Try(() => inputs[i][j] == 'X' && inputs[i - 1][j + 1] == 'M' && inputs[i - 2][j + 2] == 'A' && inputs[i - 3][j + 3] == 'S') ? 1 : 0;
+                }
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Day} {MethodName} answer: {result} - in {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        private static bool Try(Delegate del)
+        {
+            try
+            {
+                bool b = bool.Parse(del.DynamicInvoke().ToString());
+                return b;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void Part2()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string MethodName = MethodBase.GetCurrentMethod().Name;
+            List<string> inputs = IO.ReadInputAsLines(Day);
+            int result = 0;
+
+
+
+            stopwatch.Stop();
+            Console.WriteLine($"{Day} {MethodName} answer: {result} - in {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
